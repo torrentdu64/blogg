11a17e7c12d1d73553482d49e9b2553e220bd957
torrent benoit <torrentdu64@gmail.com>
2019-07-22 14:53:23 +1200

    user assisiate to posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,3 +9,13 @@ export const fetchPosts =  () => {
     })
   }
 }
+
+export const fetchUser =  (id) => {
+  return async (dispatch) => {
+   const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({
+      type: 'FETCH_USER',
+      payload: response.data
+    })
+  }
+}
